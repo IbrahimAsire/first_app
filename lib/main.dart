2cb94ca3668b675ccfbb2436e7b27d83680e3245
Ibrahim-Asiri <i-m-6@hotmail.com>
2023-01-25 19:26:08 +0300

    formating

--- lib/main.dart
+++ lib/main.dart
@@ -101,9 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             const Text(
               "This is from me to try only ... ",
-
             ),
-            //Here I add to try
             const Text(
               "try try try and practice to what si going on here after writing "
                   "this one ",
